0df9799a2c64b24bf3637315bd2a758371a283ab
adrianevandro <adrianevandro159@gmail.com>
2022-05-19 16:57:43 -0300

    Início.

---
+++ WYSIWYG/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WYSIWYG.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
